761709cd81dfc520cfba8573c8ccca75ca64198d
Siyuan Peng <peng2000@umd.edu>
2023-06-07 12:58:23 -0400

    add unconditional image genreation

---
+++ dreambooth/train_db.sh
@@ -0,0 +1,17 @@
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export INSTANCE_DIR="/Users/siyuan/Desktop/study/research/image_to_img_generation/resources/xray_images/normal"
+export OUTPUT_DIR="./output"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="a photo of human chest x-ray" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=400 \
+  --push_to_hub
