a46318cd8dca0ca3778f6b0cbf9ab956b7c10b9e
Siyuan - MD.ai <peng2000@umd.edu>
2023-06-21 17:00:21 +0000

    update requirement.txt

--- dreambooth/train_db.sh
+++ dreambooth/train_db.sh
@@ -1,12 +1,12 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export INSTANCE_DIR="/home/siyuan/research/image_to_img_generation/resources/xray_images/normal"
-export OUTPUT_DIR="./output"
+export INSTANCE_DIR="/home/siyuan/image_to_img_generation/resources/xray_images/normal"
+export OUTPUT_DIR="./output/normal"
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of human chest x-ray" \
+  --instance_prompt="x-ray of healthy human chest" \
   --resolution=1024 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
--- uncond_img_gen/train.sh
+++ uncond_img_gen/train.sh
@@ -1,11 +1,12 @@
 accelerate launch train_unconditional.py \
-  --train_data_dir="/home/siyuan/research/image_to_img_generation/resources/xray_images/normal" \
+  --train_data_dir="/home/siyuan/image_to_img_generation/resources/xray_images/normal_100" \
   --resolution=512 \
-  --output_dir="./output" \
-  --train_batch_size=16 \
-  --num_epochs=100 \
+  --output_dir="./output_100" \
+  --train_batch_size=1 \
+  --num_epochs=120 \
   --gradient_accumulation_steps=1 \
   --learning_rate=1e-4 \
   --lr_warmup_steps=500 \
   --mixed_precision=no \
-  --logger "wandb" 
+  --logger "wandb" \
+  --resume_from_checkpoint 'latest'
